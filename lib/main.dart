d9bf1940ed82c01f238939dbb047b8b82e62bb09
ahmedashraffcih <ahmedashraffcih@gmail.com>
2020-03-15 00:53:54 +0200

    App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,41 @@
 import 'package:flutter/material.dart';
 
-//void main() {
-//  runApp(MaterialApp(
-//    debugShowCheckedModeBanner: false,
-//    home: HomePage(),
-//  ));
-//}
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
 
-//class HomePage extends StatefulWidget {
-//  @override
-//  _HomePageState createState() => _HomePageState();
-//}
-//
-//class _HomePageState extends State<HomePage> {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: new AppBar(
-//        elevation: 0.1,
-//        backgroundColor: Colors.blue,
-//        title: Text('Shopping App'),
-//        actions: <Widget>[
-//          new IconButton(
-//              icon: Icon(
-//                Icons.search,
-//                color: Colors.white,
-//              ),
-//              onPressed: () {}),
-//          new IconButton(
-//              icon: Icon(
-//                Icons.shopping_cart,
-//                color: Colors.white,
-//              ),
-//              onPressed: () {})
-//        ],
-//      ),
-//      drawer: new Drawer(
-//        child: new ListView(
-//          children: <Widget>[
-////            header
-//            new UserAccountsDrawerHeader(
-//              accountName: Text('Ahmed Ashraf'),
-//              accountEmail: Text('ahmedfcih@gmail.com'),
-//              currentAccountPicture: GestureDetector(
-//                child: new CircleAvatar(
-//                  backgroundColor: Colors.red.shade800,
-//                  child: Icon(Icons.person, color: Colors.white,),
-//                ),
-//              ),
-//              decoration: new BoxDecoration(
-//                  color: Colors.red
-//              ),
-//            ),
-////            body
-//            InkWell(
-//              onTap: (){},
-//              child: ListTile(
-//                title: Text('Home Page'),
-//                leading: Icon(Icons.home),
-//              ),
-//            ),
-//
-//            InkWell(
-//              onTap: (){},
-//              child: ListTile(
-//                title: Text('My account'),
-//                leading: Icon(Icons.person),
-//              ),
-//            ),
-//
-//            InkWell(
-//              onTap: (){},
-//              child: ListTile(
-//                title: Text('My Orders'),
-//                leading: Icon(Icons.shopping_basket),
-//              ),
-//            ),
-//
-//            InkWell(
-//              onTap: (){},
-//              child: ListTile(
-//                title: Text('Categoris'),
-//                leading: Icon(Icons.dashboard),
-//              ),
-//            ),
-//
-//            Divider(),
-//
-//            InkWell(
-//              onTap: (){},
-//              child: ListTile(
-//                title: Text('Settings'),
-//                leading: Icon(Icons.settings, color: Colors.blue,),
-//              ),
-//            ),
-//
-//            InkWell(
-//              onTap: (){},
-//              child: ListTile(
-//                title: Text('About'),
-//                leading: Icon(Icons.help, color: Colors.green),
-//              ),
-//            ),
-//          ],
-//        ),
-//      ),
-//    );
-//  }
-//}
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        elevation: 0.1,
+        backgroundColor: Colors.blue,
+        title: Text('Shopping App'),
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          new IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {})
+        ],
+      ),
+
+    );
+  }
+}
