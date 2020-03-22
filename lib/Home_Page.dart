c2083f6d3abd9c4907060e9b41d1c5ce01eac90c
Ahmed Ashraf <ahmedashraffcih@gmail.com>
2020-03-22 12:46:41 +0200

    Setup Theme

--- lib/Home_Page.dart
+++ lib/Home_Page.dart
@@ -3,7 +3,6 @@ import 'Reuseable_Inkwell.dart';
 import 'package:flutter_shopping_app/Icon_Buttons.dart';
 import 'Constants.dart';
 
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -13,46 +12,89 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //APPLICATION BAR
       appBar: new AppBar(
+        //This Style for the Drawer Icon
+        //any Variable 'K' which if for constants.dart
         iconTheme: new IconThemeData(color: KPrimaryBrightColor),
+
+        //Bar Dropshadow
         elevation: 0.1,
         title: Text('Shopping App'),
+
         actions: <Widget>[
-          Icon_Buttons(icon: Icons.search,Button_color: KPrimaryBrightColor,),
-          Icon_Buttons(icon: Icons.shopping_cart,Button_color: KPrimaryBrightColor,)
+          //Search
+          Icon_Buttons(
+            icon: Icons.search,
+            Button_color: KPrimaryBrightColor,
+          ),
+          //Chart
+          Icon_Buttons(
+            icon: Icons.shopping_cart,
+            Button_color: KPrimaryBrightColor,
+          )
         ],
       ),
 
+      //Drawer or Side Bar
       drawer: new Drawer(
         child: Container(
           color: KSubDarkColor,
           child: new ListView(
             children: <Widget>[
-//            header
+//            header : DATA
               new UserAccountsDrawerHeader(
+                //Data goes here
                 accountName: Text('Ahmed Ashraf'),
                 accountEmail: Text('ahmedfcih@gmail.com'),
+
+                // User Picture
                 currentAccountPicture: GestureDetector(
                   child: new CircleAvatar(
                     backgroundColor: KSubDarkColor,
-                    child: Icon(Icons.person, color: Colors.white,),
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
+
+                //Box holding first section (User Data)
                 decoration: new BoxDecoration(
-                    color: KBoxColor,
+                  color: KBoxColor,
                 ),
               ),
-//            body
-              Reuseable_Inkwell(InkTitle: 'Home Page',icon: Icons.home,),
-              Reuseable_Inkwell(InkTitle: 'My account',icon: Icons.person,),
-              Reuseable_Inkwell(InkTitle: 'My Orders',icon: Icons.shopping_basket,),
-              Reuseable_Inkwell(InkTitle: 'Categoris',icon: Icons.dashboard,),
-              Reuseable_Inkwell(InkTitle: 'Settings',icon: Icons.settings,),
-              Reuseable_Inkwell(InkTitle: 'About',icon: Icons.help,),
+
+              //Drawer Body
+              Reuseable_Inkwell(
+                InkTitle: 'Home Page',
+                icon: Icons.home,
+              ),
+              Reuseable_Inkwell(
+                InkTitle: 'My account',
+                icon: Icons.person,
+              ),
+              Reuseable_Inkwell(
+                InkTitle: 'My Orders',
+                icon: Icons.shopping_basket,
+              ),
+              Reuseable_Inkwell(
+                InkTitle: 'Categoris',
+                icon: Icons.dashboard,
+              ),
+              Reuseable_Inkwell(
+                InkTitle: 'Settings',
+                icon: Icons.settings,
+              ),
+              Reuseable_Inkwell(
+                InkTitle: 'About',
+                icon: Icons.help,
+              ),
             ],
           ),
         ),
       ),
+
     );
   }
 }
