90bbc6e9a48accc9d3152ccc8a1962a134a9a0bb
AbdelrahmanSalama <abdelrahmansalamafcih@gmail.com>
2020-03-27 16:43:54 +0200

    Product_Details v2

--- lib/Product_Details.dart
+++ lib/Product_Details.dart
@@ -1,12 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_shopping_app/Constants.dart';
 import 'package:flutter_shopping_app/Icon_Buttons.dart';
 import 'package:flutter_shopping_app/Product_Details_Button.dart';
 
-
 class ProductDetails extends StatefulWidget {
-
-  ProductDetails({this.productDetailName,this.productDetailPicture,this.productDetailPrice});
+  ProductDetails(
+      {this.productDetailName,
+      this.productDetailPicture,
+      this.productDetailPrice});
   final productDetailName;
   final productDetailPrice;
   final productDetailPicture;
@@ -15,7 +17,6 @@ class ProductDetails extends StatefulWidget {
 }
 
 class _ProductDetailsState extends State<ProductDetails> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,11 +42,9 @@ class _ProductDetailsState extends State<ProductDetails> {
           )
         ],
       ),
-
       body: new ListView(
         children: <Widget>[
-
-          // Picture
+          // Product Picture
           Container(
             height: 300.0,
             child: Container(
@@ -55,41 +54,93 @@ class _ProductDetailsState extends State<ProductDetails> {
           ),
 
           //Name and Price
-          Container(
-            child: ListTile(
-              leading: Text(widget.productDetailName,style: KProductNameTextStyle,),
-              title: Text("\$${widget.productDetailPrice}",style: KProductPriceTextStyle,),
-            ),
-          ),
-
-          // +++++The First Button+++++
-          Row(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Product_Details_Button(textButton: "Size",),
-              Product_Details_Button(textButton: "Color",),
-              Product_Details_Button(textButton: "Qty",),
+              Padding(
+                padding:
+                    const EdgeInsets.only(left: 15.0, top: 12.0, bottom: 8.0),
+                child: Text(
+                  widget.productDetailName,
+                  style: KProductDetailsNameTextStyle,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15.0, bottom: 12.0),
+                child: Text(
+                  "${widget.productDetailPrice}\.00 EGP",
+                  style: KProductDetailsPriceTextStyle,
+                ),
+              ),
             ],
           ),
 
+          Divider(),
+
+          // +++++ Choosing Menu +++++
+          Row(
+            children: <Widget>[
+              //size
+              Product_Details_Button(
+                textButton: "Size",
+                buttonContent: "Choose Size",
+              ),
+
+              //Color
+              Product_Details_Button(
+                textButton: "Color",
+                buttonContent: "Choose Color",
+              ),
+
+              //Qty
+              Product_Details_Button(
+                textButton: "Qty",
+                buttonContent: "Choose Color",
+              ),
+            ],
+          ),
+
+          // --- BUY NOW --
           Row(
             children: <Widget>[
               // ---- Size ----
               Expanded(
                 child: MaterialButton(
-                  onPressed: (){},
-                  color: KPrimaryBrightColor,
-                  textColor: KPrimaryDarkColor,
-                  elevation: 0.2,
-                  child: Text("Buy Now")
-                ),
+                    onPressed: () {},
+                    color: KPrimaryBrightColor,
+                    textColor: KPrimaryDarkColor,
+                    elevation: 0.2,
+                    child: Text("Buy Now")),
               ),
-              new Icon_Buttons(icon: Icons.shopping_cart,Button_color: KPrimaryBrightColor,),
-              new Icon_Buttons(icon: Icons.favorite_border,Button_color: KPrimaryBrightColor,),
+              new Icon_Buttons(
+                icon: Icons.shopping_cart,
+                Button_color: KPrimaryBrightColor,
+              ),
+              new Icon_Buttons(
+                icon: Icons.favorite_border,
+                Button_color: KPrimaryBrightColor,
+              ),
             ],
+          ),
+          Divider(),
+
+
+          // PRODUCT DETAILS
+          ListTile(
+            title: Padding(
+              padding: const EdgeInsets.only(bottom: 7.0),
+              child: Text(
+                "Product Description",
+                style: KProductDescriptionTitleTextStyle,
+              ),
+            ),
+            subtitle: Text(
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+              style: KProductDescriptionTextStyle,
+            ),
           ),
         ],
       ),
     );
   }
 }
-
--- lib/Product_Details_Button.dart
+++ lib/Product_Details_Button.dart
@@ -2,23 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter_shopping_app/Constants.dart';
 
 class Product_Details_Button extends StatelessWidget {
-
-  Product_Details_Button({this.textButton});
+  Product_Details_Button({@required this.textButton,@required this.buttonContent});
   final String textButton;
+  final String buttonContent;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: MaterialButton(
-        onPressed: (){},
-        color: KBoxColor,
-        textColor: KPrimaryDarkColor,
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text(textButton),
+                  content: Text(buttonContent),
+                  actions: <Widget>[
+                    MaterialButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(context);
+                      },
+                      child: Text("CLOSE"),
+                    )
+                  ],
+                );
+              });
+        },
+        color: KPrimaryDarkColor,
+        textColor: KPrimaryBrightColor,
         elevation: 0.2,
         child: Row(
           children: <Widget>[
-            Expanded(
-                child:Text(textButton)
-            ),
+            Expanded(child: Text(textButton)),
             Expanded(
               child: Icon(Icons.arrow_drop_down),
             ),
